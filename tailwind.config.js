a13d4bd51af05659b989a430ed2dc7b1ec6e97d5
Greg <gwgh1g21@soton.ac.uk>
2024-09-09 23:14:16 +0100

    added daisyui and supabase

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        foreground: "var(--foreground)"
+      }
+    }
   },
-  plugins: [],
+  plugins: [require("daisyui")]
 };
