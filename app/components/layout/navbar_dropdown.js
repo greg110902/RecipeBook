bb894974a531a0fa9db234c3a8e27f7c7caa64a7
Greg <gwgh1g21@soton.ac.uk>
2024-09-09 23:53:46 +0100

    added tabindex to see if that closes menu on click

--- app/components/layout/navbar_dropdown.js
+++ app/components/layout/navbar_dropdown.js
@@ -1,8 +1,13 @@
 export default function NavBarDropdown({ title, content }) {
   return (
     <details className="dropdown">
-      <summary className="btn m-1">{title}</summary>
-      <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+      <summary tabIndex={0} className="btn m-1">
+        {title}
+      </summary>
+      <ul
+        tabIndex={0}
+        className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+      >
         {content.map((child) => {
           return (
             <li>
